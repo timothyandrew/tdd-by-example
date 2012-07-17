a6498edcdc0b14fe7af07d4fa62283730cf90479
Timothy Andrew <mail@timothyandrew.net>
2012-07-17 14:16:41 +0530

    First red/green/refactor.

---
+++ test/money_example_test.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/money_example.rb'
+require 'test/unit'
+
+class TestMoneyExample < Test::Unit::TestCase
+  def test_multiplication
+    five = Dollar.new(5)
+    five.times(2)
+    assert_equal(10, five.amount)
+  end
+end
