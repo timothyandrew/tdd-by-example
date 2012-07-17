6c8e5ff5494c582dcd665599c96464ebb1c11d63
Timothy Andrew <mail@timothyandrew.net>
2012-07-17 18:14:33 +0530

    Add set_up.

--- part-2/lib/xunit.rb
+++ part-2/lib/xunit.rb
@@ -3,20 +3,29 @@ class TestCase
     @name = name
   end
 
+  def set_up
+  end
+
   def run
+    set_up
     self.send @name
   end
 end
 
 class WasRun < TestCase
-  attr_accessor :was_run
+  attr_accessor :was_run, :was_set_up
 
   def initialize(name)
-    @was_run = nil
+    @was_set_up = nil
     super
   end
 
   def test_method
     @was_run = 1
+  end
+
+  def set_up
+    @was_run = nil
+    @was_set_up = 1
   end
 end
--- part-2/test/xunit_test.rb
+++ part-2/test/xunit_test.rb
@@ -5,18 +5,20 @@ def assert(expr)
 end
 
 class TestCaseTest < TestCase
+  def set_up
+    @test = WasRun.new("test_method")
+  end
+
   def test_running
-    test = WasRun.new("test_method") 
-    assert(!test.was_run)
-    test.run
-    assert test.was_run
+    @test.run
+    assert(@test.was_run)
   end
 
   def test_set_up
-    test = WasRun.new("test_method")
-    test.run
-    assert(test.was_set_up)
+    @test.run
+    assert(@test.was_set_up)
   end
 end
 
 TestCaseTest.new('test_running').run
+TestCaseTest.new('test_set_up').run
